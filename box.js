08fb46447e013f656c77a3de8612a7b07bd245e4
vihaan-chordiya <71765043+vihaan-chordiya@users.noreply.github.com>
2020-11-06 13:33:19 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,24 @@
+
+class box{
+    constructor(x,y,width,height) {
+ 
+     var option = {
+       isStatic : true
+     }
+ 
+     this.body = Bodies.rectangle(x,y,width,height,option);
+     this.width = width;
+     this.height = height;
+     World.add(world, this.body);
+ 
+     }
+ 
+     display() {
+      var pos = this.body.position;
+      fill ("red")
+      rectMode(CENTER)
+      rect(pos.x,pos.y,this.width,this.height)   
+     }
+ 
+ 
+ }
